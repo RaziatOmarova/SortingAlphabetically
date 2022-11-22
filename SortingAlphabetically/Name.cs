466a9602f5f3bb400dbd0f5b925b234e4510e689
Raziat <114012022+RaziatOmarova@users.noreply.github.com>
2022-11-22 23:11:39 +0300

    Добавьте файлы проекта.

---
+++ SortingAlphabetically/Name.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlphabetically
+{
+    internal class Name
+    {
+        public Name(string username)
+        {
+            Username = username;
+        }
+        public string Username { get; set; }
+    }
+}
---
+++ SortingAlphabetically/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlphabetically
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> users = new List<string> {"Абдулхаким", "Магомед", "Виктор", "Разият", "Екатерина", "Антон"};
+            var orderedUser = from name in users orderby name select name;
+            foreach (var name in orderedUser)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
